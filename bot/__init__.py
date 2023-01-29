b1172ada65126e11e8fca6859ca7120e201bb8a6
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-29 22:09:16 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,33 +1,92 @@
 import os
 from telegram.ext import Updater
 from config import Config
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+import warnings
+import threading
+import sys
+sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
 
-try:
-	BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-	print(BASE_DIR)
-	s = BASE_DIR + '/usr/lib/chromium-browser/chromedriver'
-	print (s)
-	updater = Updater(token = Config.BOT_TOKEN, use_context=True)
-	dp = updater.dispatcher
-	options = webdriver.FirefoxOptions()
-	options.add_argument("-profile")
-	options.add_argument(s)
-	#profile = FirefoxProfile()
-	#profile.set_preference("dom.webdriver.enabled", False)
-	#profile.set_preference('useAutomationExtension', False)
-	#profile.set_preference("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/60.0.3112.50 Safari/537.36")
-	#profile.update_preferences()
-	desired = DesiredCapabilities.FIREFOX
-	options.add_argument("--disable-infobars")
-	#options.add_argument("--headless")
-	#options.add_argument("--window-size=1200,800")
-	options.add_argument("--disable-blink-features=AutomationControlled")
-	#browser = webdriver.Firefox(options=options, firefox_profile=profile, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
-	browser = webdriver.Firefox(options=options, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
-	#browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-except Exception as e:
-	print(e)
+from faker import Faker
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
+
+proxylist = [
+    "192.99.101.142:7497",
+    "198.50.198.93:3128",
+    "52.188.106.163:3128",
+    "20.84.57.125:3128",
+    "172.104.13.32:7497",
+    "172.104.14.65:7497",
+   "165.225.220.241:10605",
+    "165.225.208.84:10605",
+    "165.225.39.90:10605",
+    "165.225.208.243:10012",
+    "172.104.20.199:7497",
+    "165.225.220.251:80",
+    "34.110.251.255:80",
+    "159.89.49.172:7497",
+    "165.225.208.178:80",
+    "205.251.66.56:7497",
+    "139.177.203.215:3128",
+    "64.235.204.107:3128",
+    "165.225.38.68:10605",
+    "165.225.56.49:10605",
+    "136.226.75.13:10605",
+    "136.226.75.35:10605",
+    "165.225.56.50:10605",
+    "165.225.56.127:10605",
+    "208.52.166.96:5555",
+    "104.129.194.159:443",
+    "104.129.194.161:443",
+    "165.225.8.78:10458",
+    "5.161.93.53:1080",
+    "165.225.8.100:10605",
+]
+
+warnings.filterwarnings('ignore')
+fake = Faker('en_IN')
+MUTEX = threading.Lock()
+
+
+def sync_print(text):
+    with MUTEX:
+        print(text)
+
+
+
+def get_driver(proxy):
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_experimental_option("detach", True)
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-setuid-sandbox") 
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu") 
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    if proxy is not None:
+        options.add_argument(f"--proxy-server={proxy}")
+    driver = webdriver.Chrome('chrome',options=options)
+    return driver
+
+
+def driver_wait(driver, locator, by, secs=10, condition=ec.element_to_be_clickable):
+    wait = WebDriverWait(driver=driver, timeout=secs)
+    element = wait.until(condition((by, locator)))
+    return element
