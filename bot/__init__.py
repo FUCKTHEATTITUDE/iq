16c0e8b7f54846910e84e36449244d918ab195e1
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-29 21:50:21 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,22 +1,19 @@
 import os
 from telegram.ext import Updater
 from config import Config
-from .restricted import restricted
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-service = Service(executable_path=ChromeDriverManager().install())
-from selenium.webdriver.common.by import By
-
-
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 try:
-	
-	
+	BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+	print(BASE_DIR)
+	s = BASE_DIR + '/Profile/'
+	print (s)
 	updater = Updater(token = Config.BOT_TOKEN, use_context=True)
 	dp = updater.dispatcher
-	options = webdriver.ChromeOptions()
+	options = webdriver.FirefoxOptions()
 	options.add_argument("-profile")
 	options.add_argument(s)
 	#profile = FirefoxProfile()
@@ -28,10 +25,9 @@ try:
 	options.add_argument("--disable-infobars")
 	#options.add_argument("--headless")
 	#options.add_argument("--window-size=1200,800")
-	options = Options()
 	options.add_argument("--disable-blink-features=AutomationControlled")
 	#browser = webdriver.Firefox(options=options, firefox_profile=profile, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
-	browser = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options ,desired_capabilities=desired, service_args=["--marionette-port", "2828"])
+	browser = webdriver.Firefox(options=options, desired_capabilities=desired, service_args=["--marionette-port", "2828"])
 	#browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 except Exception as e:
 	print(e)
