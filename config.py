551723659b48be42625fb56f0baca7abf6c77102
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-07 12:07:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '5912024718:AAEH4ps9yHW3tLIf54oK8djR_3_Pg6yIUag')
     SCHEDULE = os.environ.get('SCHEDULE', False)
-    USERID = os.environ.get('USERID', '1930954213')
+    USERID = os.environ.get('USERID', '1963180144')
 
 # If you're not familiar with how to set Environment Variables you can do like this instead
 # of  setting Environment Variables
